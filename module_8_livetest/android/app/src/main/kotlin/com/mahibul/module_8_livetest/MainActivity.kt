b32d94b9aca3c8c721ce7c7e2a170ffa1c89b47c
Mahibul Hassan <mahibulhassan@Mahibuls-MacBook-Pro.local>
2023-07-03 22:40:08 +0600

    Module 8 Live Test basement Added

---
+++ module_8_livetest/android/app/src/main/kotlin/com/mahibul/module_8_livetest/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mahibul.module_8_livetest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ module_8_livetest/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Module 8 Live Test',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  //Variable Part
+
+
+  //Code Part
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Module 8 Live Test"),
+      ),
+      body: Center(
+        child: Container(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed:(){
+
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
