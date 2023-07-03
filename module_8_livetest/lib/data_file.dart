30523aeca03792f77f1c81a41d0701b9ca43fca0
Mahibul Hassan <mahibulhassan@Mahibuls-MacBook-Pro.local>
2023-07-03 23:29:46 +0600

    module 8 live test done

---
+++ module_8_livetest/lib/data_file.dart
@@ -0,0 +1,10 @@
+
+
+class Contact {
+  String name;
+  String email;
+  String number;
+
+  Contact(this.name,this.email,this.number);
+
+}
--- module_8_livetest/lib/main.dart
+++ module_8_livetest/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'data_file.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -15,6 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
+      debugShowCheckedModeBanner: false,
       home: const MyHomePage(),
     );
   }
@@ -30,25 +33,56 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
   //Variable Part
-
+  List<Contact> contacts =[
+    Contact("Mahibul Hassan", "mahibulhassan842@gmail.com", "01681690327"),
+    Contact("Emon", "mahibul61@gmail.com", "01981690300"),
+    Contact("Mahibul ", "hassan842@gmail.com", "01681690857"),
+    Contact("Hassan", "mahibul842@gmail.com", "01684581235"),
+    Contact("Mahir", "mahibulhassan842@gmail.com", "01681690327"),
+  ];
 
   //Code Part
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("Module 8 Live Test"),
+        title:const Center(child: Text("Contact List")),
       ),
       body: Center(
-        child: Container(),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed:(){
-
-        },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: ListView.builder(
+          itemCount: contacts.length,
+          itemBuilder: (context,index){
+            return ListTile(
+              title: Text(contacts[index].name),
+              onTap: (){
+                Scaffold.of(context).showBottomSheet<void>(
+                    (BuildContext context){
+                      return Container(
+                        height: size.height*0.3,
+                        width: size.width*1,
+                        color: Colors.white70,
+                        child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text("Contact Details",style: TextStyle(fontSize: 20,color: Colors.black),),
+                              const SizedBox(height: 20,),
+                              Text("Name : ${contacts[index].name}",style: const TextStyle(fontSize: 15,color: Colors.black)),
+                              Text("Email : ${contacts[index].email}",style:const TextStyle(fontSize: 15,color: Colors.black)),
+                              Text("Phone Number : ${contacts[index].number}",style:const TextStyle(fontSize: 15,color: Colors.black)),
+                            ],
+                          ),
+                        ),
+                      );
+                    }
+                );
+              },
+            );
+          },
+        ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
