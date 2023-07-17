b2b3c5060280b0f1f3c84d12a7cc6d944f0b11ae
Mahibul Hassan <mahibulhassan@Mahibuls-MacBook-Pro.local>
2023-07-17 23:36:56 +0600

    module 10 live test done

--- module_10_livetest/lib/main.dart
+++ module_10_livetest/lib/main.dart
@@ -34,13 +34,70 @@ class _MyHomePageState extends State<MyHomePage> {
   //Code Part
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: Text("Module 10 Live Test"),
       ),
-      body: Container(
-        child: Text("Nothing"),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: MediaQuery.of(context).orientation == Orientation.portrait ? potredMode(size):landScape(size) , // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Widget potredMode(Size size){
+    return ListView.builder(itemCount: 15,
+    itemBuilder: (context , index){
+      return Column(
+        children: [
+         Container(
+            width: size.width*1,
+            height: size.height*0.2,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.white70,
+            ),
+            child: Center(
+              child: Container(
+                color: Colors.black12,
+                child: SizedBox(
+                  height: 150,
+                  width: 150,
+                  child: Center(child: Text("150 * 150")),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10,)
+        ],
+      );
+    },);
+  }
+
+  Widget landScape (Size size){
+    return GridView.builder(
+      itemCount: 6,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2) , itemBuilder: (context,index){
+      return  Row(
+        children: [
+          Container(
+            width: size.width*0.48,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.white70,
+            ),
+            child: Center(
+              child: Container(
+                color: Colors.black12,
+                child: SizedBox(
+                  height: 150,
+                  width: 150,
+                  child: Center(child: Text("150 * 150")),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(width: 10,)
+        ],
+      );
+    });
   }
 }
